ffef19b8ad2b5ce77fb21c71a38b46fa964f8088
Nick Gottlieb <ngottlieb@gmail.com>
2018-11-22 15:47:46 -0700

    update map with style and property popups

--- app/map.js
+++ app/map.js
@@ -15,8 +15,29 @@ export default class Map {
   }
 
   loadDataset() {
-    this.geojsonLayer = new L.GeoJSON.AJAX("https://raw.githubusercontent.com/loganpowell/census-geojson/master/GeoJSON/500k/2017/american-indian-area!alaska-native-area!hawaiian-home-land.json").
+    this.geojsonLayer = new L.GeoJSON.AJAX("https://raw.githubusercontent.com/loganpowell/census-geojson/master/GeoJSON/500k/2017/american-indian-area!alaska-native-area!hawaiian-home-land.json",
+        {
+          onEachFeature: this.onEachFeature,
+          style: this.setRandomFeatureColor
+        }).
       addTo(this.map);
   }
 
+  onEachFeature(feature, layer) {
+    if (feature.properties && feature.properties.NAME) {
+      layer.bindPopup(feature.properties.NAME);
+    }
+  }
+
+  setRandomFeatureColor(feature) {
+    // from https://stackoverflow.com/questions/1484506/random-color-generator#1484514
+    const letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return {
+      color: color
+    };
+  }
 }
